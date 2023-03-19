4a146cd03d42ba12c75a4d7c6143e276d57afa88
Tasin Shafi Leon <leontasinshafi@gmail.com>
2023-03-19 16:16:42 +0600

    email verification middleware

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -9,9 +9,9 @@
                 <div class="col-lg-12">
                     <div class="text-center mb-5 text-muted">
                         <a href="{{ route('home') }}" class="d-block auth-logo">
-                            <img src="{{ url('img/logo-dark.png') }}" alt="" height="20"
+                            <img src="{{ url('img/logo-dark.png') }}" alt="" height="120"
                                 class="auth-logo-dark mx-auto">
-                            <img src="{{ url('img/logo-light.png') }}" alt="" height="20"
+                            <img src="{{ url('img/logo-light.png') }}" alt="" height="120"
                                 class="auth-logo-light mx-auto">
                         </a>
                         <p class="mt-3">Doinik News Portal</p>
@@ -33,7 +33,7 @@
                                     <div class="p-2 mt-4">
                                         <h4>{{ __('Verify Your Email Address') }}</h4>
                                         @if (session('resent'))
-                                            <p>{{ __('A fresh verification link has been sent to your email address.') }}
+                                            <p>{{ __('A fresh verification link has been sent to your email address. Please check '). Auth::user()->email }}.
                                             </p>
                                             <div class="mt-4">
                                                 <a href="{{ route('home') }}" class="btn btn-success w-md">Verify email</a>
@@ -46,7 +46,6 @@
                     </div>
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <input type="hidden" name="token" value="{{ $token }}">
                         <div class="mt-5 text-center">
                             <p>Didn't receive an email ? <button type="submit"
                                     class="fw-medium text-primary">{{ __('click here to request another') }}</button>.</p>
